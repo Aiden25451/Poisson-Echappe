034d4435ae2b7bb749e6053fe7576bdb02942e04
Aiden25451 <a.asprakis@gmail.com>
2023-05-26 18:55:34 -0400

    0.11
    First Kind of Working Version

--- poissonEchappe/poissonEchappe/settings.py
+++ poissonEchappe/poissonEchappe/settings.py
@@ -144,10 +144,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_DIRS = [BASE_DIR / 'static']
+if DEBUG:
+    STATIC_URL = 'static/'
+else:
+    STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
