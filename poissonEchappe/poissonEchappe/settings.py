01961390a9804c6dd6835979586162f7a6dc2b71
Aiden25451 <a.asprakis@gmail.com>
2023-05-31 23:27:01 -0400

    0.15
    So close

--- poissonEchappe/poissonEchappe/settings.py
+++ poissonEchappe/poissonEchappe/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-if not DEBUG:
+if DEBUG:
     STATIC_URL = 'static/'
 else:
     STATIC_URL = 'static/'
